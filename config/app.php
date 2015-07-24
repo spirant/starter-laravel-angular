641ff05392891a83ef9a92d8decd963259bae470
timothy head <tjchead@gmail.com>
2015-07-24 12:23:43 +0100

    Initial Commit

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
 	| stack traces will be shown on every error that occurs within your
 	| application. If disabled, a simple generic error page is shown.
 	|
-	*/
+	 */
 
 	'debug' => env('APP_DEBUG'),
 
@@ -24,7 +24,7 @@ return [
 	| the Artisan command line tool. You should set this to the root of
 	| your application so that it is used when running Artisan tasks.
 	|
-	*/
+	 */
 
 	'url' => 'http://localhost',
 
@@ -37,7 +37,7 @@ return [
 	| will be used by the PHP date and date-time functions. We have gone
 	| ahead and set this to a sensible default for you out of the box.
 	|
-	*/
+	 */
 
 	'timezone' => 'UTC',
 
@@ -50,7 +50,7 @@ return [
 	| by the translation service provider. You are free to set this value
 	| to any of the locales which will be supported by the application.
 	|
-	*/
+	 */
 
 	'locale' => 'en',
 
@@ -63,7 +63,7 @@ return [
 	| is not available. You may change the value to correspond to any of
 	| the language folders that are provided through your application.
 	|
-	*/
+	 */
 
 	'fallback_locale' => 'en',
 
@@ -76,9 +76,9 @@ return [
 	| to a random, 32 character string, otherwise these encrypted strings
 	| will not be safe. Please do this before deploying an application!
 	|
-	*/
+	 */
 
-	'key' => env('APP_KEY', 'SomeRandomString'),
+	'key' => env('APP_KEY', 'md4QbuhEU5KKcPAPOugVI7CJ7dQnv9q0'),
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
@@ -93,7 +93,7 @@ return [
 	|
 	| Available Settings: "single", "daily", "syslog", "errorlog"
 	|
-	*/
+	 */
 
 	'log' => 'daily',
 
@@ -106,7 +106,7 @@ return [
 	| request to your application. Feel free to add your own services to
 	| this array to grant expanded functionality to your applications.
 	|
-	*/
+	 */
 
 	'providers' => [
 
@@ -162,43 +162,43 @@ return [
 	| is started. However, feel free to register as many as you wish as
 	| the aliases are "lazy" loaded so they don't hinder performance.
 	|
-	*/
+	 */
 
 	'aliases' => [
 
-		'App'       => 'Illuminate\Support\Facades\App',
-		'Artisan'   => 'Illuminate\Support\Facades\Artisan',
-		'Auth'      => 'Illuminate\Support\Facades\Auth',
-		'Blade'     => 'Illuminate\Support\Facades\Blade',
-		'Bus'       => 'Illuminate\Support\Facades\Bus',
-		'Cache'     => 'Illuminate\Support\Facades\Cache',
-		'Config'    => 'Illuminate\Support\Facades\Config',
-		'Cookie'    => 'Illuminate\Support\Facades\Cookie',
-		'Crypt'     => 'Illuminate\Support\Facades\Crypt',
-		'DB'        => 'Illuminate\Support\Facades\DB',
-		'Eloquent'  => 'Illuminate\Database\Eloquent\Model',
-		'Event'     => 'Illuminate\Support\Facades\Event',
-		'File'      => 'Illuminate\Support\Facades\File',
-		'Hash'      => 'Illuminate\Support\Facades\Hash',
-		'Input'     => 'Illuminate\Support\Facades\Input',
+		'App' => 'Illuminate\Support\Facades\App',
+		'Artisan' => 'Illuminate\Support\Facades\Artisan',
+		'Auth' => 'Illuminate\Support\Facades\Auth',
+		'Blade' => 'Illuminate\Support\Facades\Blade',
+		'Bus' => 'Illuminate\Support\Facades\Bus',
+		'Cache' => 'Illuminate\Support\Facades\Cache',
+		'Config' => 'Illuminate\Support\Facades\Config',
+		'Cookie' => 'Illuminate\Support\Facades\Cookie',
+		'Crypt' => 'Illuminate\Support\Facades\Crypt',
+		'DB' => 'Illuminate\Support\Facades\DB',
+		'Eloquent' => 'Illuminate\Database\Eloquent\Model',
+		'Event' => 'Illuminate\Support\Facades\Event',
+		'File' => 'Illuminate\Support\Facades\File',
+		'Hash' => 'Illuminate\Support\Facades\Hash',
+		'Input' => 'Illuminate\Support\Facades\Input',
 		'Inspiring' => 'Illuminate\Foundation\Inspiring',
-		'Lang'      => 'Illuminate\Support\Facades\Lang',
-		'Log'       => 'Illuminate\Support\Facades\Log',
-		'Mail'      => 'Illuminate\Support\Facades\Mail',
-		'Password'  => 'Illuminate\Support\Facades\Password',
-		'Queue'     => 'Illuminate\Support\Facades\Queue',
-		'Redirect'  => 'Illuminate\Support\Facades\Redirect',
-		'Redis'     => 'Illuminate\Support\Facades\Redis',
-		'Request'   => 'Illuminate\Support\Facades\Request',
-		'Response'  => 'Illuminate\Support\Facades\Response',
-		'Route'     => 'Illuminate\Support\Facades\Route',
-		'Schema'    => 'Illuminate\Support\Facades\Schema',
-		'Session'   => 'Illuminate\Support\Facades\Session',
-		'Storage'   => 'Illuminate\Support\Facades\Storage',
-		'URL'       => 'Illuminate\Support\Facades\URL',
+		'Lang' => 'Illuminate\Support\Facades\Lang',
+		'Log' => 'Illuminate\Support\Facades\Log',
+		'Mail' => 'Illuminate\Support\Facades\Mail',
+		'Password' => 'Illuminate\Support\Facades\Password',
+		'Queue' => 'Illuminate\Support\Facades\Queue',
+		'Redirect' => 'Illuminate\Support\Facades\Redirect',
+		'Redis' => 'Illuminate\Support\Facades\Redis',
+		'Request' => 'Illuminate\Support\Facades\Request',
+		'Response' => 'Illuminate\Support\Facades\Response',
+		'Route' => 'Illuminate\Support\Facades\Route',
+		'Schema' => 'Illuminate\Support\Facades\Schema',
+		'Session' => 'Illuminate\Support\Facades\Session',
+		'Storage' => 'Illuminate\Support\Facades\Storage',
+		'URL' => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
-		'View'      => 'Illuminate\Support\Facades\View',
-		'JWTAuth'   => 'Tymon\JWTAuth\Facades\JWTAuth',
+		'View' => 'Illuminate\Support\Facades\View',
+		'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
 
 	],
 
